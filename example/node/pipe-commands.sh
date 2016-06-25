a1960561e3bf19cc9ac4e6d5eea684d60fc6812a
John Henry <john@iamjohnhenry.com>
2016-06-25 09:04:50 -0700

    Fixed examples

--- example/node/pipe-commands.sh
+++ example/node/pipe-commands.sh
@@ -2,4 +2,4 @@ alias duckduckgo-query='pipe-dreams node example/node/pull/query.js --init.searc
 alias google-query='pipe-dreams node example/node/pull/query.js --init.search="https://www.google.com/search?q="'
 alias extract-title='pipe-dreams node example/node/pull/extract-title.js -u title'
 alias prompt-next='pipe-dreams node example/node/pull/prompt-next.js'
-alias search='duckduckgo-query'
+alias search='duckduckgo-query | extract-title | prompt-next'
--- example/node/pushpull/pushpull-modules.sh
+++ example/node/pushpull/pushpull-modules.sh
@@ -1,1 +1,1 @@
-alias push-pull='pipe-dreams module example/module/pushpull/pushpull.js'
+alias push-pull='pipe-dreams node example/node/pushpull/pushpull.js'
